27100bd36aa4a20348c77fec2dd59c958364cbee
Krystin Fields <krysfields12@gmail.com>
2022-01-11 16:13:11 -0500

    file name change,

--- server/db/models/Movie.js
+++ server/db/models/Movie.js
@@ -9,22 +9,18 @@ const Movie = db.define('movie', {
             notEmpty: true
         }
     },
-
     imageUrl: {
         type: Sequelize.STRING,
         defaultValue: 'https://res.cloudinary.com/people-matters/image/upload/f_auto,q_auto,w_800,h_800,c_lpad/v1517845732/1517845731.jpg'
     },
-
     description: {
         type: Sequelize.TEXT,
         defaultValue: 'Currently Unavailable'
     },
-
     genre: {
         type: Sequelize.STRING,
         defaultValue: 'Unknown'
     },
-
     price: {
         type: Sequelize.DECIMAL(10,2),
         allowNull: false,
