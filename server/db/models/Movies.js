1c749dc1edc3631c7a5c3ac0ae4546b2ed70f9d1
Krystin Fields <krysfields12@gmail.com>
2022-01-11 12:55:13 -0500

    movie model completed

---
+++ server/db/models/Movies.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const db = require('..db');
+
+const Movie = db.define('movie', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://res.cloudinary.com/people-matters/image/upload/f_auto,q_auto,w_800,h_800,c_lpad/v1517845732/1517845731.jpg'
+    },
+
+    description: {
+        type: Sequelize.TEXT
+    },
+
+    genre: {
+        type: Sequelize.STRING
+    },
+    
+    price: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false,
+    }
+})
+
+module.export = Movie
+
