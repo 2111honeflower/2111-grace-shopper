5e60125b7e97333eb2f34eb5da0513f3ddf875ca
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-12 08:19:41 -0600

    initial commit

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,12 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import moviesReducer from './movies'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({
+  auth,
+  movies: moviesReducer
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
