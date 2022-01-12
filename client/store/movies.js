845935e2cabddc56a4d411504dd23c1839e36e6c
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-12 16:01:37 -0600

    added delete redux route

--- client/store/movies.js
+++ client/store/movies.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 //action types
 const SET_MOVIES = 'SET_MOVIES';
+const DELETE_MOVIE = 'DELETE_MOVIE'
 
 //action creators
 export const setMovies = (movies) => ({
   type: SET_MOVIES,
   movies
+});
+
+const _deleteMovie = (movie) => ({
+  type: DELETE_MOVIE,
+  movie
 });
 
 //thunks
@@ -15,11 +21,23 @@ export const fetchMovies = () => {
     try {
       const {data} = await axios.get('/api/movies');
       dispatch(setMovies(data));
-    } catch (error){
-      console.log(error);
+    } catch (err){
+      console.log(err);
     }
   };
 };
+
+export const deleteMovie = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data: movie } = await axios.delete(`/api/movies/${id}`);
+      dispatch(_deleteMovie(movie));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
 
 const initialState = [];
 
@@ -27,6 +45,8 @@ export default function moviesReducer(state = initialState, action) {
   switch (action.type) {
     case SET_MOVIES:
       return action.movies;
+    case DELETE_MOVIE:
+      return state.filter(movie => movie.id !== action.movie.id);
     default:
       return state;
   }
