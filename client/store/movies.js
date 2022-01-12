fb60a19949aae532111732bd92a1a861fdb1159e
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-12 09:20:01 -0600

    completed movies get reducer

---
+++ client/store/movies.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+//action types
+const SET_MOVIES = 'SET_MOVIES';
+
+//action creators
+export const setMovies = (movies) => ({
+  type: SET_MOVIES,
+  movies
+});
+
+//thunks
+
+export const fetchMovies = () => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.get('/api/movies');
+      dispatch(setMovies(data));
+    } catch (error){
+      console.log(error);
+    }
+  };
+};
+
+const initialState = [];
+
+export default function moviesReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_MOVIES:
+      return action.movies;
+    default:
+      return state;
+  }
+}
