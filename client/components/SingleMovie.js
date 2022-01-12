9f327efec22faeade4d1e063c8949f116ea6c2fe
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-12 11:53:40 -0600

    added map state and dipatch to single movie component

--- client/components/SingleMovie.js
+++ client/components/SingleMovie.js
@@ -4,3 +4,23 @@ import { Link } from 'react-router-dom';
 
 import { fetchSingleMovie } from "../store/singleMovie";
 
+class SingleMovie extends React.Component {
+
+  componentDidMount(){
+    this.props.getMovie(this.props.match.params.id);
+  }
+
+  render(){
+
+  }
+}
+
+const mapState = (state) => ({
+  movie: state.movie
+});
+
+const mapDispatch = (dispatch) => ({
+  getStudent: (id) => dispatch(fetchSingleMovie(id))
+});
+
+export default connect(mapState, mapDispatch)(SingleMovie);
