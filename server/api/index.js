4065fbc08c0012272dd2af2bb9ac983da92738c6
Krista Naso <90427458+knaso17@users.noreply.github.com>
2022-01-13 10:14:45 -0600

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 
 router.use("/users", require("./users"));
 router.use("/cart", require("./cart"));
+router.use("/movies", require("./movies"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
