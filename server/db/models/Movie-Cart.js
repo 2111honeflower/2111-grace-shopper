1a1ec314b99eb376889e10d5fb5ad6885e52e1f6
Krystin Fields <krysfields12@gmail.com>
2022-01-15 12:54:05 -0500

    add to cart work, but not working yet

---
+++ server/db/models/Movie-Cart.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Movie_Cart = db.define('Movie_Cart', {
+    quantity: {
+        type: Sequelize.INTEGER
+    },
+    //Cara's note was to have pivot table have a snap shot of cart
+    //populate upon purchase/places status change - clarification needed. 
+
+})
+
+module.exports = Movie_Cart;
