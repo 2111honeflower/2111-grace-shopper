c43ab25c35d1454b14b32d126930b34bc113178e
Cara Dodge <shxpxshxftxr@gmail.com>
2022-01-18 13:01:08 -0500

    quantity feature in cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -7,8 +7,9 @@ import { fetchCartMovies } from "../store/movies";
 export class Cart extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
-    this.deleteFromCart = this.deleteFromCart.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.deleteFromCart = this.deleteFromCart.bind(this);
+    this.changeQty = this.changeQty.bind(this);
   }
   componentDidMount() {
     // event.preventDefault()
@@ -16,18 +17,39 @@ export class Cart extends React.Component {
     // this.props.loadMovies(this.props.match.params.id);
   }
   handleClick(id, event) {
-    event.preventDefault()
-    this.deleteFromCart(id)
-    this.forceUpdate()
+    event.preventDefault();
+    this.deleteFromCart(id);
+    this.forceUpdate();
   }
+  handleSubmit(id, event) {
+    event.preventDefault();
+    this.changeQty(id, event);
+    this.forceUpdate();
+  }
+  changeQty(id, event) {
+    let cartMovies = JSON.parse(localStorage.getItem("products"));
+    let change = document.getElementById(event.target.id).innerHTML;
+
+    cartMovies.filter((movie) => {
+      if (movie.id === id) {
+        let quantity = Number(movie.qty);
+        if (change === "+") {
+          quantity += 1;
+        } else {
+          quantity -= 1;
+        }
+        movie.qty = `${quantity}`;
+      }
+    });
+    localStorage.setItem("products", JSON.stringify(cartMovies));
+  }
+
   deleteFromCart(id) {
     let cartMovies = JSON.parse(localStorage.getItem("products"));
-    console.log(cartMovies, "cartMovies")
-    let movies = cartMovies.filter((movie) => movie.id !== id);
-    console.log(movies, "movies filtered")
-    localStorage.setItem("products", JSON.stringify(movies));
-    console.log(localStorage, "local storage")
 
+    let movies = cartMovies.filter((movie) => movie.id !== id);
+
+    localStorage.setItem("products", JSON.stringify(movies));
   }
 
   render() {
@@ -49,18 +71,32 @@ export class Cart extends React.Component {
                       <h3>{movie.name}</h3>
                     </Link>
 
-                    <h4>${movie.price}</h4>
+                    <h3>${movie.price}</h3>
+                    <div id="qty-buttons">
+                      <h3 id="cart-qty">Qty: {movie.qty}</h3>
+                      <button
+                        type="submit"
+                        id="+"
+                        onClick={(event) => this.handleSubmit(movie.id, event)}
+                      >
+                        +
+                      </button>
+                      <button
+                        type="submit"
+                        id="-"
+                        onClick={(event) => this.handleSubmit(movie.id, event)}
+                      >
+                        -
+                      </button>
+                    </div>
 
-                    <h4>QUANTITY HERE</h4>
-
-
-                    <button type="submit" onClick={(event) => this.handleClick(movie.id, event)} id="delete-button">
-                      <img
-                        src="https://www.shareicon.net/data/512x512/2016/09/10/827820_delete_512x512.png"
-
-                      />
+                    <button
+                      type="submit"
+                      onClick={(event) => this.handleClick(movie.id, event)}
+                      id="delete-button"
+                    >
+                      <img src="https://www.shareicon.net/data/512x512/2016/09/10/827820_delete_512x512.png" />
                     </button>
-
                   </div>
                 );
               })
@@ -68,12 +104,8 @@ export class Cart extends React.Component {
           {/* <p>SUB-TOTAL:{cart.totalPrice}</p> */}
         </div>
         {cartMovies.length > 0 ? (
-          <Link
-            to={`/checkout`}
-            id="checkout-button"
-          >
-            <button type="submit">Proceed to Checkout
-            </button>
+          <Link to={`/checkout`} id="checkout-button">
+            <button type="submit">Proceed to Checkout</button>
           </Link>
         ) : (
           ""
