ffc844bf08886e803f3ccd82f9382337b033477d
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-18 14:31:38 -0600

    notes from help desk

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,11 +1,13 @@
-const {
-  models: { Movie, Cart, Movie_Cart },
-} = require('../db');
+const { models: { Movie, Cart, Movie_Cart },} = require('../db');
 const router = require('express').Router();
 
-//GUEST
 
 router.post('/:id', async (req, res, next) => {
+  //id is user id
+  //if cart, add item using movie_cart with movieId and Quantity
+  //send those items
+
+  //should be able to push local storage with this route as well
   try {
     const [userCart, created] = await Cart.findOrCreate({
       where: {
@@ -13,6 +15,7 @@ router.post('/:id', async (req, res, next) => {
         userId: req.params.userId
       },
     });
+    //we're always going to send movie data to movie_cart
     if (created) {
       res.json(userCart);
     } else {
@@ -23,100 +26,4 @@ router.post('/:id', async (req, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const userCart = await Cart.findOne({
-      where: {
-        id: req.params.id,
-        status: 'Open',
-      },
-    });
-    res.json(userCart);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const userCart = await Cart.findOne({
-      where: {
-        id: req.params.id,
-        status: 'Open',
-      },
-    });
-    res.json(await userCart.update(req.body));
-  } catch (err) {
-    next(err);
-  }
-});
-
-//MOVIE-CART
-router.get('/:id/movie-cart', async (req, res, next) => {
-  try {
-    const movieIds = await Movie_Cart.findAll({
-      where: {
-        cartId: req.params.id,
-      },
-    });
-    const movies = await movieIds.map((movie) => {
-      Movie.findOne({
-        where: {
-          movieId: movie.movieId,
-        },
-      });
-    });
-    res.json(movies);
-  } catch (err) {
-    next(err);
-  }
-});
-
-//MEMBER
-
-// router.post("/:id/:userId", async (req, res, next) => {
-//   try {
-//     const [ userCart, created ] = await Cart.findOrCreate( {where: {
-//       userId: req.params.userId,
-//       id: req.params.id,
-//       status: "open"
-//     }})
-//     if (created) {
-//       res.json(userCart)
-//     } else {
-//       res.json("Cannot have two open carts") // can we merge carts? or is this a case of please sign in?
-//     }
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// router.get("/:id/:userId", async (req, res, next) => {
-//   try {
-//     const userCart = await Cart.findOne( {where: {
-//       id: req.params.id,
-//       userId: req.params.userId,
-//       status: "Open"
-//     }})
-//     res.json(userCart)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// router.put("/:id/:userId", async (req, res, next) => {
-//   try {
-//     const userCart = await Cart.findOne( {where: {
-//       id: req.params.id,
-//       userId: req.params.userId,
-//       status: "Open"
-//     }})
-//   res.json(await userCart.update(req.body))
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-//is cart with id when guest, and then /:id/:userId when logged in?
-//does cart model need guest id?
-//no deleting of cart since we will use 'placed' to 'clear cart '
 module.exports = router;
