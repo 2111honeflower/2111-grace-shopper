577633335150e761a20a6ffbba7da1090e6a826b
Cara Dodge <shxpxshxftxr@gmail.com>
2022-01-20 04:49:27 -0500

    css on confirm page

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -2,29 +2,28 @@ import React from "react";
 import { connect } from "react-redux";
 
 class Confirmation extends React.Component {
-    render() { 
-        const { username } = this.props || "Guest"
-        const thankYou = 'https://us.123rf.com/450wm/redcollegiya/redcollegiya1709/redcollegiya170900336/86730240-thank-you-lettering-vector-handwritten-text-card-template.jpg?ver=6'
-        const confirmationNumber = Math.floor(Math.random() * 10000000000)
-        return (
-            <div>
-                <div>
-                    <img src= {thankYou} style={{width: '1000px', height: '300px'}}/>
-                </div>
-                <div style={{ position: 'absolute', left: 200, color: 'black' }}>
-                    <h1>Thank you for your purchase, {username}!</h1>
-                    <h3>Your order number is: {confirmationNumber}</h3>
-                </div>
-            </div>
-        )
-     }      
-   }
+  render() {
+    const username = this.props.username || "Guest";
+    const thankYou =
+      "https://us.123rf.com/450wm/redcollegiya/redcollegiya1709/redcollegiya170900336/86730240-thank-you-lettering-vector-handwritten-text-card-template.jpg?ver=6";
+    const confirmationNumber = Math.floor(Math.random() * 10000000000);
+    return (
+      <div id="confirm">
+        <img src={thankYou} />
 
-   const mapState = (state) => {
-    return {
-      username: state.auth.userName,
-    };
+        <div id="ty-message">
+          <h1>Thank you for your purchase, {username}!</h1>
+          <h2>Your order number is: {confirmationNumber}</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapState = (state) => {
+  return {
+    username: state.auth.userName,
   };
-  
-  export default connect(mapState, null)(Confirmation);
-  
+};
+
+export default connect(mapState, null)(Confirmation);
