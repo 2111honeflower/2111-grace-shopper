33fd66c2f3f680d50e13ced0f84b3601ccee7dc7
Cara Dodge <shxpxshxftxr@gmail.com>
2022-03-25 13:52:31 -0400

    deleted movie-shopper

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -47,7 +47,7 @@ module.exports = User;
  * instanceMethods
  */
 User.prototype.correctPassword = function (candidatePwd) {
-  //we need to compare the plain version to an encrypted version of the password
+
   return bcrypt.compare(candidatePwd, this.password);
 };
 
